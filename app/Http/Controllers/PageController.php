e3d6c456969a1c954fab1c5ab91571ace28292ba
GiuliaV <valente.giulia93@gmail.com>
2022-07-14 17:27:51 +0200

    creato collegamento db

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -7,6 +7,15 @@ use Illuminate\Http\Request;
 class PageController extends Controller
 {
     public function index(){
-        return view('welcome');
+        $data = [
+            'pageName' => 'Homepage',
+            'pageTitle' => 'Test'
+        ];
+
+        return view('home', $data);
+    }
+
+    public function movies(){
+        return view('movies');
     }
 }
