59203746fe1b25a9f5eabef4b6168e7ab3d179c5
GiuliaV <valente.giulia93@gmail.com>
2022-07-14 18:43:49 +0200

    aggiunto css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,23 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/app.css">
     <title>Home</title>
 </head>
-<body>
-     
-                
-    @foreach($movies as $movie)
-    <article>
-        <p>Titolo: {{ $movie['title'] }}</p> 
-        <p>Titolo originale: {{ $movie['original_title'] }}</p>
-        <p>Nazionalità: {{ $movie['nationality'] }}</p>
-        <p>Anno: {{ $movie['date'] }}</p>
-        <p>Voto: {{ $movie['vote'] }}</p>
-    </article>
-    @endforeach
+<body> 
+    <header>
+        <h1>Film</h1>   
+    </header>   
 
+    <main>          
+        @foreach($movies as $movie)
+        <article>
+            <p>Titolo: {{ $movie['title'] }}</p> 
+            <p>Titolo originale: {{ $movie['original_title'] }}</p>
+            <p>Nazionalità: {{ $movie['nationality'] }}</p>
+            <p>Anno: {{ $movie['date'] }}</p>
+            <p>Voto: {{ $movie['vote'] }}</p>
+        </article>
+        @endforeach
+    </main> 
     
-</div>
-</div>
 </body>
 </html>
